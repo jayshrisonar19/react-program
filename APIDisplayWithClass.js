951bc2cda8f7265262e40eda60b2140f0f5ebe39
jayshrisonar19 <147793246+jayshrisonar19@users.noreply.github.com>
2023-12-07 19:49:06 +0530

    APIDisplayWithClass program

---
+++ APIDisplayWithClass.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class APIDisplayWithClass extends Component {
+
+
+    state={
+        data : []
+    }
+
+    componentDidMount()
+    {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(y=>y.json())
+        .then(y=>{
+
+            this.setState({...this.state, data:y});
+        })
+    }
+  render() {
+    return (
+        <table>{this.state.data.map((v)=>{
+  
+          return (<tr><td>{v.userId}</td><td>{v.body}</td></tr>)
+  
+        })}</table>
+      )
+    }
+  }

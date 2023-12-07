4a02181d86a966bf8c1b3ce3b6bdf0f3ef340e8e
jayshrisonar19 <147793246+jayshrisonar19@users.noreply.github.com>
2023-12-07 19:52:10 +0530

    EDisplay program

---
+++ EDisplay.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+export const EDisplay = ({title,img,des}) => {
+  return (
+    <Col xs lg="3">
+    <Card style={{ width: '18rem' }}>
+    <Card.Img variant="top" src={img} />
+    <Card.Body>
+      <Card.Title>{title}</Card.Title>
+      <Card.Text>
+        {des}
+      </Card.Text>
+      <Button variant="primary">Go somewhere</Button>
+    </Card.Body>
+  </Card>
+  </Col>
+  )
+}
